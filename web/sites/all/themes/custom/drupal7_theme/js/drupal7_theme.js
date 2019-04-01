a962ff42736be7739d48f37578a962b686d21ba7
Phong Phan <phongphandinhphong@gmail.com>
2019-04-01 07:50:02 +0700

    Test simple ajax with delivery callback

---
+++ web/sites/all/themes/custom/drupal7_theme/js/drupal7_theme.js
@@ -0,0 +1,54 @@
+/**
+ * @file
+ * Contains the definition of the behaviour artistics.
+ */
+
+(function ($, Drupal) {
+
+    'use strict';
+  
+    Drupal.behaviors.drupal7_theme = {
+      attach: function (context, settings) {
+        
+        $('ul > li.first.leaf').on('click', function(e) {
+            $('#phannphong').load('/node/get/ajax/19381');
+            e.preventDefault();
+        });
+      }
+  
+    };
+  
+    $(document).ready(function(context) {
+      $('body').on('submit', function(e) {
+        
+        $('form').find('.messages.error').remove();
+        var $recaptchaResponse = $('body').find('#g-recaptcha-response');
+
+        if ($recaptchaResponse.length && $recaptchaResponse.val() == "") {
+          var htmlError = '<div class="messages error">The recaptcha.</div>';
+          $('form').prepend(htmlError);
+          $('<span class="red recaptcha">reCAPTCHA is invalid</span>').insertAfter('.g-recaptcha');
+          e.preventDefault();
+        } else {
+          var phone = '';
+          var testPhone = /^\d+$/;
+          var $phoneInput = $(this).find('.webform-component--phone > input');
+
+          if ($phoneInput.length) {
+            phone = $phoneInput.val();
+          }
+          if (testPhone.test(phone)) {
+          }
+          else {
+            console.log('test');
+            var htmlError = '<div class="messages error">' + Drupal.t('The telephone number should only contain numbers. Please correct.') + '</div>';
+            $('form').prepend(htmlError);
+            e.preventDefault();
+          }
+        }
+
+      })
+    });
+  
+  })(jQuery, Drupal);
+  
